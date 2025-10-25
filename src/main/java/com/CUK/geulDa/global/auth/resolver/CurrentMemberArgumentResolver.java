348946290480ef2b1de52f9faf530b836c37fbde
margie1a <4987kk@naver.com>
2025-10-25 22:42:40 +0900

    refactor: 기존의 memberId 방식에서 member 주입 방식 전환

--- src/main/java/com/CUK/geulDa/global/auth/resolver/CurrentMemberArgumentResolver.java
+++ src/main/java/com/CUK/geulDa/global/auth/resolver/CurrentMemberArgumentResolver.java
@@ -1,8 +1,11 @@
 package com.CUK.geulDa.global.auth.resolver;
 
+import com.CUK.geulDa.domain.member.Member;
+import com.CUK.geulDa.domain.member.service.MemberService;
 import com.CUK.geulDa.global.apiResponse.code.ErrorCode;
 import com.CUK.geulDa.global.apiResponse.exception.BusinessException;
 import com.CUK.geulDa.global.auth.annotation.CurrentMember;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
 import org.springframework.security.core.Authentication;
@@ -14,17 +17,23 @@ import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
 /**
- * @CurrentMember 어노테이션이 붙은 파라미터에 현재 로그인한 사용자의 ID를 자동으로 주입하는 Resolver
+ * @CurrentMember 어노테이션이 붙은 파라미터에 현재 로그인한 사용자 정보를 자동으로 주입하는 Resolver
+ * - Long 타입: memberId만 반환
+ * - Member 타입: Member 엔티티 전체 반환 (DB 조회)
  */
 @Component
+@RequiredArgsConstructor
 @Slf4j
 public class CurrentMemberArgumentResolver implements HandlerMethodArgumentResolver {
 
+    private final MemberService memberService;
+
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
-        // @CurrentMember 어노테이션이 있고, 타입이 Long인 경우에만 지원
+        // @CurrentMember 어노테이션이 있고, 타입이 Long 또는 Member인 경우 지원
         return parameter.hasParameterAnnotation(CurrentMember.class) &&
-               Long.class.isAssignableFrom(parameter.getParameterType());
+               (Long.class.isAssignableFrom(parameter.getParameterType()) ||
+                Member.class.isAssignableFrom(parameter.getParameterType()));
     }
 
     @Override
@@ -45,7 +54,21 @@ public class CurrentMemberArgumentResolver implements HandlerMethodArgumentResol
         }
 
         Long memberId = (Long) authentication.getPrincipal();
-        log.debug("@CurrentMember 주입: memberId={}", memberId);
-        return memberId;
+
+        // Long 타입이면 memberId만 반환
+        if (Long.class.isAssignableFrom(parameter.getParameterType())) {
+            log.debug("@CurrentMember 주입 (Long): memberId={}", memberId);
+            return memberId;
+        }
+
+        // Member 타입이면 Member 엔티티 조회 후 반환
+        if (Member.class.isAssignableFrom(parameter.getParameterType())) {
+            Member member = memberService.getMemberById(memberId);
+            log.debug("@CurrentMember 주입 (Member): memberId={}, email={}", memberId, member.getEmail());
+            return member;
+        }
+
+        // 여기 도달하면 안 됨 (supportsParameter에서 걸러짐)
+        throw new IllegalStateException("지원하지 않는 파라미터 타입입니다.");
     }
 }
